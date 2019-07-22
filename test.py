6e87a9a1dee8249d9e268ef0eaf45b7749efceac
hmm <huo.mei.mei@163.com>
2019-07-22 17:00:15 +0800

    saaa

--- test.py
+++ test.py
@@ -23,11 +23,18 @@ if __name__ == "__main__":
     # print(a)
 
 
-    str = '<div class="para" label-module="para">（下一站：<a target=_blank href="/item/%E7%A6%8F%E5%AF%BF%E5%B2%AD%E7%AB%99/1827061" data-lemmaid="1827061">福寿岭站</a>）</div>' \
-          '<div class="para" label-module="para">距离2606米</div>'
-    pattern = r".*\（下一站：<a.*>(\w+)</a>\）.*距离(\d+)米.*"
+
+    str = '（下一站：<a target=_blank href="/item/%E7%A6%8F%E5%AF%BF%E5%B2%AD%E7%AB%99/1827061" data-lemmaid="1827061">福寿岭站</a>）</div>\r\n<div class="para" label-module="para">（预留车站，暂未启用）</div>\r\n</td><td align="center" valign="middle" colspan="3" rowspan="3"><div class="para" label-module="para"><a target=_blank href="/item/%E5%8C%97%E4%BA%AC%E5%9C%B0%E9%93%811%E5%8F%B7%E7%BA%BF/7646800" data-lemmaid="7646800">北京地铁1号线</a></div>\r\n</td><td align="center" valign="middle" colspan="3" rowspan="3"><div class="para" label-module="para">开往<a target=_blank href="/item/%E5%9B%9B%E6%83%A0%E4%B8%9C%E7%AB%99">四惠东站</a></div>\r\n<div class="para" label-module="para">（下一站：<a target=_blank href="/item/%E5%8F%A4%E5%9F%8E%E7%AB%99/2662893" data-lemmaid="2662893">古城站</a>）</div>\r\n<div class="para" label-module="para">距离2606米'
+
+    # pattern = r".*?（下一站：<a.*?>(\w+)</a>）.*?(距离(\d+)米|(（\w+）)).*"
+    # pattern = r".*（下一站：<a.*?>(\w+)</a>）.*(距离(\d+)米).*"
+    # pattern = r".*(（下一站：.*）(.|\n)*(距离(\d+)米)).*"
+    pattern = r"(.|\r\n)*（下一站：<a.*>(\w+)</a>）</div>(.|\r\n)*距离(\d+)米(.|\r\n)*"
     aa = re.findall(pattern, str)
     print(aa)
+    # matches = re.finditer(pattern, str)
+    # results = [match.group(1) for match in matches]
+    # print(results)
 
 
 
